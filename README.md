919e9601fddf831ab7a42328232d3f82af399066
Pierre Karpov <push.default>
2018-09-12 17:54:47 +0800

    crop rescale up and down

--- README.md
+++ README.md
@@ -6,17 +6,25 @@ For this project, we will get data from [Data.gov.sg](https://data.gov.sg/develo
 
 ## Project pipeline
 
-1. Get and store images from [traffic images API](https://data.gov.sg/dataset/traffic-images) [DONE]
-2. Automatically and periodically get data from [traffic images API](https://data.gov.sg/dataset/traffic-images) for all locations, and all times.
-3. Build tool to classify part of an image [DONE]
-4. Go through our image library, classify a lot of examples car/no car
-5. Train CNN to detect whether car/no car
-6. Go through our image library, label how many cars in each picture.
-7. Test CNN detect the correct number of cars
-8. Use the images from [traffic images API](https://data.gov.sg/dataset/traffic-images) for the times and locations of my commute specifically.
-9. Build time series with how many cars, at what time, and where.
-10. Run LSTM algorithm to estimate whether or not there will be traffic today.
-11. Snooze peacefully 😴😴😴
+1. [x] Get and store images from [traffic images API](https://data.gov.sg/dataset/traffic-images)
+2. [ ] Automatically and periodically get data from [traffic images API](https://data.gov.sg/dataset/traffic-images) for all locations, and all times.
+3. [x] Build tool to classify part of an image
+4. [x] Go through our image library, classify a lot of examples car/no car
+5. [x] Go through classified data, crop and resize selctions out of original pictures
+6. [x] Write code to build, train, and test my own deep NNs
+7. [ ] Train CNN to detect whether car/no car
+8. [ ] Go through our image library, label how many cars in each picture.
+9. [ ] Test CNN detect the correct number of cars
+10. [ ] Use the images from [traffic images API](https://data.gov.sg/dataset/traffic-images) for the times and locations of my commute specifically.
+11. [ ] Build time series with how many cars, at what time, and where.
+12. [ ] Run LSTM algorithm to estimate whether or not there will be traffic today.
+13. [ ] Snooze peacefully 😴😴😴
 
 ## Comparison analysis
 All the algorithms will be build from scratch. Once the project is finished and running, it would be interesting to compare how our algorithms hold versus the standard libraries'.
+
+
+## Other approaches to try
+* Webcrawler to get traffic data for google maps
+* Perform image difference (background extraction) to count how many cars
+* Time commute everyday and interpolate from there how long it takes
--- scripts/crop_selections.py
+++ scripts/crop_selections.py
@@ -1,10 +1,10 @@
-from PIL import Image
 from os import listdir
 from os.path import isfile, join
 import csv
 from time import time
+from PIL import Image
 
-def crop(data, in_file_path, out_file_path, out_size=32, rewrite=False):
+def crop(data, in_file_path, out_file_path, rewrite=False, out_size=32):
     if not rewrite and isfile(out_file_path):
         return 0
 
@@ -14,14 +14,16 @@ def crop(data, in_file_path, out_file_path, out_size=32, rewrite=False):
     cropped_img = img.crop(area)
 
     s = (out_size, out_size)
-    cropped_img.thumbnail(s, Image.ANTIALIAS)
-    cropped_img.save(out_file_path)
+
+    resized_cropped_img = cropped_img.resize(s, Image.ANTIALIAS)
+    resized_cropped_img.save(out_file_path)
     return 1
 
 
-def process_file(file_name):
+def process_file(file_name, rewrite=False):
     written_pics_per_file = 0
     file_path = "image_data/image_selections/" + file_name + ".jpg.csv"
+
     with open(file_path, 'rb') as csvfile:
         datareader = csv.reader(csvfile, delimiter=' ', quotechar='|')
         cars_idx = 0
@@ -37,29 +39,33 @@ def process_file(file_name):
                 out_file_path = out_file_path + "/not_car/" + file_name + "_not_car_" + str(not_cars_idx) + ".jpg"
                 not_cars_idx = not_cars_idx + 1
 
-            pic_written = crop(d[1:], in_file_path, out_file_path)
+            pic_written = crop(d[1:], in_file_path, out_file_path, rewrite)
             written_pics_per_file = written_pics_per_file + pic_written
+
     return written_pics_per_file
 
-def generate_selections():
+def generate_selections(rewrite=False):
     written_files = 0
     image_selection_files = [f for f in listdir("./image_data/image_selections") if isfile(join("./image_data/image_selections", f))]
+
     for f in image_selection_files:
         file_name = f.split(".")[0]
-        written_pics_per_file = process_file(file_name)
+        written_pics_per_file = process_file(file_name, rewrite)
         written_files = written_files + written_pics_per_file
+
     return written_files
 
 def count_cropped_files():
     car_files = [f for f in listdir("./image_data/cropped_images/car") if isfile(join("./image_data/cropped_images/car", f))]
     not_car_files = [f for f in listdir("./image_data/cropped_images/not_car") if isfile(join("./image_data/cropped_images/not_car", f))]
+
     return len(car_files), len(not_car_files)
 
 if __name__ == "__main__":
     t0 = time()
     written_files = generate_selections()
     t1 = time()
-
-    print  "Wrote %d cropped selections in %f s" %(written_files, (t1 - t0))
     cs, ncs = count_cropped_files()
+    
+    print  "Wrote %d cropped selections in %f s" %(written_files, (t1 - t0))
     print "There are %d cars and %d not cars cropped selections" %(cs, ncs)
